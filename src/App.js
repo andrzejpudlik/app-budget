e1bc1e49b241df4499def71d2691f9f0bda83f0f
andrzejpudlik <andrzejpudlik98@gmail.com>
2021-01-25 12:27:01 +0100

    Create basic action to fetchbudget data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Button, LoadingIndicator, Navigation, Wrapper } from 'components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { connect } from 'react-redux';
+import { fetchBudget, fetchBudgetedCategories } from 'data/actions/budget.actions';
 import theme from 'utils/theme';
 
 import { GlobalStyles }  from './index.css';
 
-function App() {
+function App({ budget, fetchBudget, fetchBudgetedCategories }) {
+
+  useEffect(() => {
+    fetchBudget(1);
+    fetchBudgetedCategories(1);
+  }, [fetchBudget, fetchBudgetedCategories]);
+
   const { i18n } = useTranslation();
+
   return (
     <React.Fragment>
       <GlobalStyles />
@@ -36,11 +45,20 @@ function App() {
   );
 }
 
+const ConnectedApp = connect(state => {
+  return {
+    budget: state.budget.budget
+  }
+}, {
+    fetchBudget,
+    fetchBudgetedCategories,
+})(App);
+
 function RootApp() {
   return (
     <ThemeProvider theme={theme}>
       <React.Suspense fallback={<LoadingIndicator />}>
-        <App />
+        <ConnectedApp />
       </React.Suspense>
     </ThemeProvider>
   )
