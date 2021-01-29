b7694d21dcf742c54ce8901f983d373eff223f61
andrzejpudlik <andrzejpudlik98@gmail.com>
2021-01-29 13:57:59 +0100

    create budget component and shifting logic to this component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Button, LoadingIndicator, Navigation, Wrapper } from 'components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
-import { fetchBudget, fetchBudgetedCategories } from 'data/actions/budget.actions';
 import theme from 'utils/theme';
+import Budget from 'pages/Budget';
 
 import { GlobalStyles }  from './index.css';
 
 function App({ budget, fetchBudget, fetchBudgetedCategories }) {
 
-  useEffect(() => {
-    fetchBudget(1);
-    fetchBudgetedCategories(1);
-  }, [fetchBudget, fetchBudgetedCategories]);
+
 
   const { i18n } = useTranslation();
 
@@ -35,8 +32,12 @@ function App({ budget, fetchBudget, fetchBudgetedCategories }) {
         />
         <Wrapper>
           <Switch>
-            <Route path="/budget">Budget page</Route>
-            <Route exact path="/">Home page</Route> 
+            <Route exact path="/">
+              Home page
+            </Route> 
+            <Route path="/budget">
+              <Budget />
+            </Route>
           </Switch>
         </Wrapper>
       </Router>
@@ -45,20 +46,12 @@ function App({ budget, fetchBudget, fetchBudgetedCategories }) {
   );
 }
 
-const ConnectedApp = connect(state => {
-  return {
-    budget: state.budget.budget
-  }
-}, {
-    fetchBudget,
-    fetchBudgetedCategories,
-})(App);
 
 function RootApp() {
   return (
     <ThemeProvider theme={theme}>
       <React.Suspense fallback={<LoadingIndicator />}>
-        <ConnectedApp />
+        <App />
       </React.Suspense>
     </ThemeProvider>
   )
--- src/data/reducers/common.reducer.js
+++ src/data/reducers/common.reducer.js
@@ -1,5 +1,4 @@
 import { 
-    ALL_CATEGORIES_GET,
     ALL_CATEGORIES_GET_REQUEST,
     ALL_CATEGORIES_GET_SUCCESS,
     ALL_CATEGORIES_GET_FAILURE,
@@ -7,11 +6,11 @@ import {
   } from 'data/constants';
   
   const initialState = {
-    loadingState: {},
+    loadingState: null,
     allCategories: [],
   }
   
-  function budget(state = initialState, action) {
+  function common(state = initialState, action) {
     const newLoadingState = { ...state.loadingState };
   
     switch(action.type) {
@@ -47,4 +46,4 @@ import {
     }
   }
   
-  export default budget;
+export default common;
